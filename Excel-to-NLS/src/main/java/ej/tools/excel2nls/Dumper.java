dff99fcd023d2343f39ec9e1ce691c17b276885d
Gaëtan Harel <gharel@is2t-intern.com>
2016-06-20 09:49:58 +0200

    Revert "merged from develop"
    This reverts commit 8e6f4f81a43008377e33552b1a34ec6019bbf46e

---
+++ Excel-to-NLS/src/main/java/ej/tools/excel2nls/Dumper.java
@@ -0,0 +1,77 @@
+/*
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be found at http://www.is2t.com/open-source-bsd-license/.
+ */
+package ej.tools.excel2nls;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
+/**
+ * A Dumper creates the NLS files from a set of NLS languages.
+ */
+public class Dumper {
+
+	private static final String FILENAME_FORMAT = "%s_%s.nls";
+
+	private final File outputFolder;
+	private final String header;
+	private final List<Language> languages;
+
+	/**
+	 * Instantiate a new dumper. It has:
+	 * <ul>
+	 * <li>an output folder, where it will create the files,
+	 * <li>a NLS header, that will be included in the file names,
+	 * <li>a list of {@link Language}s.
+	 * </ul>
+	 *
+	 * @param outputFolder
+	 *            the output folder
+	 * @param header
+	 *            the NLS header
+	 * @param languages
+	 *            the list of languages
+	 */
+	public Dumper(File outputFolder, String header, List<Language> languages) {
+		super();
+		this.outputFolder = outputFolder;
+		this.header = header;
+		this.languages = languages;
+	}
+
+	/**
+	 * Dump each language to its own file. If files exists, there are overridden.
+	 */
+	public void dump() {
+		for (Language language : languages) {
+			// Create the file
+			String fileName = String.format(FILENAME_FORMAT, header, language.getLocale());
+			File file = new File(outputFolder, fileName);
+
+			try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"))) {
+
+				// First line contains the display name of the language
+				writer.write("DISPLAY_NAME=" + language.getDisplayName());
+				writer.newLine();
+				System.out.println(language.getDisplayName());
+
+				// Following lines contain the messages with their IDs
+				List<Message> messages = language.getMessages();
+				for (Message message : messages) {
+					writer.write(message.getId() + "=" + message.getText());
+					writer.newLine();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ Excel-to-NLS/src/main/java/ej/tools/excel2nls/SheetReader.java
@@ -0,0 +1,76 @@
+/*
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be found at http://www.is2t.com/open-source-bsd-license/.
+ */
+package ej.tools.excel2nls;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ * A SheetReader is able to extract an NLS language from an Excel sheet.
+ * 
+ * It knows the internal structure of the sheet and hence is able to create a {@link Language} for it. See the example file
+ * (resources/translations.xlsx) to understand the structure that each sheet must have.
+ */
+public class SheetReader {
+
+	// Coordinates of the cell that contains the display name of the language
+	private static final int DISPLAY_NAME_ROW_INDEX = 0;
+	private static final int DISPLAY_NAME_COLUMN_INDEX = 1;
+
+	// Coordinates of the cell that contains the locale of the language
+	private static final int LOCALE_ROW_INDEX = 1;
+	private static final int LOCALE_COLUMN_INDEX = 1;
+
+	// // Coordinates to find the cells that contain messages
+	private static final int FIRST_MESSAGE_ROW_INDEX = 3;
+	private static final int ID_COLUMN_INDEX = 0;
+	private static final int MESSAGE_COLUMN_INDEX = 1;
+
+	private final Sheet sheet;
+
+	/**
+	 * Instantiate a new sheet reader.
+	 *
+	 * @param sheet
+	 *            the sheet
+	 */
+	public SheetReader(Sheet sheet) {
+		super();
+		this.sheet = sheet;
+	}
+
+	/**
+	 * Read the sheet and extract the NLS language it contains.
+	 * 
+	 * @return an NLS language
+	 */
+	public Language read() {
+		Language language = new Language();
+		Row row;
+
+		// Extract language
+		row = sheet.getRow(DISPLAY_NAME_ROW_INDEX);
+		String displayName = row.getCell(DISPLAY_NAME_COLUMN_INDEX).getStringCellValue();
+		language.setDisplayName(displayName);
+
+		// Extract local
+		row = sheet.getRow(LOCALE_ROW_INDEX);
+		String locale = row.getCell(LOCALE_COLUMN_INDEX).getStringCellValue();
+		language.setLocale(locale);
+
+		// Extract messages
+		int i = FIRST_MESSAGE_ROW_INDEX;
+		while ((row = sheet.getRow(i++)) != null) {
+			String id = row.getCell(ID_COLUMN_INDEX).getStringCellValue();
+			String text = row.getCell(MESSAGE_COLUMN_INDEX).getStringCellValue();
+			Message message = new Message(id, text);
+			language.add(message);
+		}
+
+		return language;
+	}
+}
